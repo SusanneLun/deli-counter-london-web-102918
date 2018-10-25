c0f59f4ac85265bebcb2874ef1e508b77603cb1a
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-25 10:10:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,11 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else 
-  katz_deli.each_with_index do |value, index| 
-    message +=  #{index.to_i+1}. #{value}
-  puts "The line is currently:#{message}"
+  katz_deli.each_with_index { |customer, index| 
+    customer_array <<  #{index + 1}. #{customer}
+  puts "The line is currently" + customer_array.join(" ")
 end 
 end 
 
+katz_deli_line.each.with_index { |customer, idx| customers_string << "#{idx + 1}. #{customer}" }
+    puts "The line is currently: " + customers_string.join(" ")
