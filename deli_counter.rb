25f6c14a307d953edfd6edf85fde95ed70264022
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-25 10:23:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
   else 
   katz_deli.each_with_index { |customer, index| 
     customer_array <<  "#{index + 1}. #{customer}"}
-   puts "The line is currently " + customer_array.join(" ")
+   puts "The line is currently: " + customer_array.join(" ")
 end 
 end 
 
