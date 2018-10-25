989146a4fc8b01c409678525c34c68b389ddeb32
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-25 10:06:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,9 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else 
-    puts message = 
   katz_deli.each_with_index do |value, index| 
     message +=  #{index.to_i+1}. #{value}
-  end 
   puts "The line is currently:#{message}"
 end 
 end 
-line(katz-deli)
+
