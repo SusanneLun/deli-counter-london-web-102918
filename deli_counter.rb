b5dcb79f2933ca12661124d1e5d48fbcf8ce3488
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-25 09:59:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 
 
-def line(name, index) 
+def line(name) 
   if name.length == 0 
     puts "The line is currently empty."
   else 
     puts message = "The line is currently:"
     
-  array.each_with_index do |value, index| 
+  name.each_with_index do |value, index| 
     message += " #{index.to_i+1}. #{value}"
   end 
   puts "#{message}"
